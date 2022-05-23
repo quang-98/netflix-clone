a9e8584f0755b2df7e7d8f9b97a0493718b84a32
quang-98 <81545697+quang-98@users.noreply.github.com>
2022-05-24 01:18:13 +0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <h1 className="text-white">Hello world !</h1>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
