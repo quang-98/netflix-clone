782158dd2e1aeb6f63ef3f3c717c9f77feb290c9
quang-98 <81545697+quang-98@users.noreply.github.com>
2022-05-25 00:56:57 +0700

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Main from "../components/Main";
+import Row from "../components/Row";
+import requests from "../Request";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <Main />
+      <Row title="Up coming" fetchURL={requests.requestAction} />
+    </>
+  );
 };
 
 export default Home;
