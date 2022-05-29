f61f5cbb554fd3b094f29bbf7acefba48fd43591
quang-98 <81545697+quang-98@users.noreply.github.com>
2022-05-30 01:09:37 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import { Route, Routes } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import { selectUser } from "./features/userSlice";
 import Home from "./pages/Home";
+import Information from "./pages/Information";
+import Login from "./pages/Login";
 
 function App() {
+  const user = useSelector(selectUser);
+  console.log(user);
+
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/information" element={<Information />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
-    </>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Information.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Information() {
+  return <div className="text-white">Information</div>;
+}
+
+export default Information;
